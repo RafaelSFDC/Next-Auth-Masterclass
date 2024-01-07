040d67fb4bcb90af31a7d15f356626a1b55d8afc
Rafael <rafaelhibiki@gmail.com>
2024-01-07 17:48:26 -0300

    feat(New Feature): :sparkles: CardWrapper and Cpmponents

---
+++ components/auth/CardWrapper.tsx
@@ -0,0 +1,44 @@
+"use client";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardContent,
+} from "@/components/ui/card";
+import Header from "./Header";
+import Social from "./Social";
+import BackButton from "./BackButton";
+type CardWrapperProps = {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+};
+
+const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      {children}
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CardWrapper;
