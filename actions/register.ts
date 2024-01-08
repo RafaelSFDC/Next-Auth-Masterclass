df96a368e8d4a42c21d8fe9f2ae4a993e269168c
Rafael <rafaelhibiki@gmail.com>
2024-01-07 21:02:47 -0300

    feat(New Feature): :sparkles: Prisma, Neon SQL, Register

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,33 @@
 "use server";
 
 import * as z from "zod";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { getUserByEmail } from "@/data/user";
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     throw new Error("Invalid Fields");
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+  // TODO SEND VERIFICATION TOKEN
+
   return { sucess: "Email and password are correct" };
 };
